8df3f7b56e3251190020a057da4a4a348f4ef661
lcspaiva87 <lcspaiva87@gmail.com>
2021-06-24 03:37:22 -0300

    creation of firebase configuration file

---
+++ src/services/firebase.ts
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+const firebaseConfig ={
+    apiKey: "AIzaSyCinDniRjfn-ft-n4rtLGuaTFcBSN1urLs",
+    authDomain: "letmeask-d440a.firebaseapp.com",
+    databaseURL: "https://letmeask-d440a-default-rtdb.firebaseio.com",
+    projectId: "letmeask-d440a",
+    storageBucket: "letmeask-d440a.appspot.com",
+    messagingSenderId: "427354402156",
+    appId: "1:427354402156:web:067a00e48c51ee55e11749"
+}
+firebase.initializeApp(firebaseConfig)
+
+export const auth = firebase.auth()
+export const databse = firebase.database()
